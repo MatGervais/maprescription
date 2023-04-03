8d652386320a3a721e3ccdf1333c846bc5359f2e
Mathieu Gervais <matgervais96@gmail.com>
2023-04-03 13:46:44 +0200

    Fix issue med reducer

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -13,6 +13,7 @@ const Item = ({name,stock,renewed,prescPerDay,id,setMedication,medication, remov
       function toRenewFunction(){
         var nbJourRestant = Math.trunc(stock/prescPerDay)
         setRenew(moment(moment(renewed).add(nbJourRestant, 'days').format("YYYY-MM-DDTHH:mm:ss.SSSSZ"))._i)
+        console.log(new Date(renewed).getTime());
         /* ----------------------------À GARDER POUR LE BACK------------------------- */
         /*       console.log(moment(new Date(toRenew).getTime()).format("DD"));       */
         /* -------------------------------------------------------------------------- */
@@ -31,7 +32,7 @@ const Item = ({name,stock,renewed,prescPerDay,id,setMedication,medication, remov
     }
     
     return (
-      <div className={`card ${daysRemaining < 10 ? "bg-danger text-white":daysRemaining < 18 ? "border-warning" : ""} col-md-4 m-3`}>
+      <div className={`card ${daysRemaining < 10 ? "bg-danger text-white":daysRemaining < 18 ? "border-warning" : ""} col-md-3 m-3`}>
         <div className= {`card-body`} >
           <div className="d-flex flex-row-reverse">
               <button type="button" onClick={removeFunction} className={`btn ${daysRemaining < 10 ? "btn-light text-danger":"btn-danger"}`} id={id} data-bs-dismiss="alert"><i className="fa fa-trash-can mx-1"></i></button>
@@ -40,7 +41,7 @@ const Item = ({name,stock,renewed,prescPerDay,id,setMedication,medication, remov
           {!edit ? (
             <>
             <h4 className="card-title" id={`${id}-${name}`}>{name}</h4>
-            <h6 className={`card-subtitle mb-2 ${daysRemaining < 10 ? "text-white":daysRemaining < 18 ? "text-warning" : "text-muted"}`} id={`${id}-${stock}`}>RESTANTS : <strong>{stock}</strong></h6>
+            <h6 className={`card-subtitle mb-2 ${daysRemaining < 10 ? "text-white":daysRemaining < 18 ? "text-warning" : "text-muted"}`} id={`${id}-${stock}`}>RESTANTS : <strong>{daysRemaining * prescPerDay}</strong></h6>
               <p className="card-text" id={`${id}-${prescPerDay}`}>Posologie (par jour) : <strong>{prescPerDay}</strong></p>
             <p className="card-text">Inventaire fait le : <strong onClick={()=>console.log("Input")}>{new Date(renewed).toLocaleDateString()}</strong> avec <strong>{stock}</strong> restants</p>
             <p className={`card-text`}><strong>Rupture de stock le : {new Date(toRenew).toLocaleDateString()} <span className={`${daysRemaining < 10 ? "text-white":daysRemaining < 18 ? "text-warning" : "text-muted"}`}>(dans {daysRemaining?daysRemaining:""} jours) </span></strong></p>
